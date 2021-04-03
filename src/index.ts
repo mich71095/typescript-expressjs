b5093620c15a72e0cdf02b7fbd88c8e3f8f72140
Michael <michaelpsy71095@gmail.com>
2021-04-04 02:23:33 +0800

    controllers

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import { itemsRouter } from "./items/items.router";
 
 dotenv.config(); // connected to .env
 
@@ -25,6 +26,7 @@ const app : any = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use("/api/menu/items", itemsRouter);
 
 
 /**
